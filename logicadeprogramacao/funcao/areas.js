a7795de37570c0e594df9b8745f6837719a6d14c
Dev. Pablo H <146978584+devpabloh@users.noreply.github.com>
2024-02-20 16:20:59 -0300

    atualizações

--- logicadeprogramacao/funcao/areas.js
+++ logicadeprogramacao/funcao/areas.js
@@ -6,3 +6,19 @@ function calcularAreaRetangulo(l, a){
     return  l * a
 }
 
+function calcularAreaTriangulo(b, a){
+
+    if(typeof l !== "number" || typeof a !== "number"){
+        throw new Error ("calculadoraAreaTriangulo aceita dois parâmetros do tipo number")
+    }
+    return  b * a / 2
+}
+
+function calcularAreaCirculo (r) {
+    if (typeof r !== "number"){
+        throw new error ("calcularAreaCirculo: Raio precisa ser um número")
+    }
+    return Math.PI * r * r
+}
+
+
