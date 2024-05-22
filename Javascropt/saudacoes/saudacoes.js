2df9df3232882d809a33124b3bc4b5aee74544f3
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-22 08:55:50 -0300

    atualização

--- Javascropt/saudacoes/saudacoes.js
+++ Javascropt/saudacoes/saudacoes.js
@@ -1,7 +1,17 @@
+/* Usando o .textcontent a gente não consegue adicionar tags ao que está sendo escrito */
+/* Já usando o innerHTML, conseguimos adicionar tags ao HTML */
+
+
+
 (function(){
-const nomeUsuario = "Pablo"
-const elemento = document.querySelector(".top-bar p")
+    const nomeUsuario = null
+    const elemento = document.querySelector(".top-bar p")
+    if(nomeUsuario){
+        elemento.innerHTML += `<b> ${nomeUsuario}</b>`
+    }else{
+        elemento.style.display = "none"
+    }
 /* elemento.textContent = `${elemento.textContent} ${nomeUsuario}!` */
-/* elemento.textContent =+ nomeUsuario */
-elemento.innerHTML += `<b> ${nomeUsuario}</b>`
+/* elemento.textContent =+ nomeUsuario */ 
+
 })()
