920879da802e2bdd6108ff0918bbdd0851a57465
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-23 17:09:13 -0300

    Update newsletter1.js

--- DOM_newsletter_original/js/newsletter1.js
+++ DOM_newsletter_original/js/newsletter1.js
@@ -4,4 +4,10 @@ const txtEmail1 = document.getElementById("txtEmail1")
 function editarEmail(){
     txtEmail1.disabled = false;
 
+
 }
+
+function disabledEmail(){
+    txtEmail1.disabled = true;
+}
+
