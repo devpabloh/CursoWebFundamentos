aaec1e60de1c7eb433a277f0fa99abd9c92e4e97
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-27 00:28:57 -0300

    Update array05.js

--- Javascropt/arrays/array05.js
+++ Javascropt/arrays/array05.js
@@ -1,2 +1,10 @@
 let arr = [1,2,3]
 console.log(arr.reverse()) //usado para inverter o fluxo original do array, lembrar que ao usar ele o seu array original é modificado
+
+let soma = arr.reduce(function(acumulador, atual){ // O método .reduce(function(acumulador, atual){}) é usado para reduzir os valores do array em apenas um número. É usado geralmete para fazer soma, tirar média.
+    return acumulador + atual
+})
+
+console.log(soma)
+
+const nomes = [ "Pablo", "Thiago", "Deyvson", "David"]
