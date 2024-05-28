97fd7b3b18607158d62690558ae49dd5e6df8b2d
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-28 12:58:29 -0300

    Update desafio_ES5.js

--- Javascropt/arrays/desafio_ES5.js
+++ Javascropt/arrays/desafio_ES5.js
@@ -1,4 +1,4 @@
-function sum(){
+function sum(){ 
     const numbers = []
     for(let i = 0; i < arguments.length; i++){
         numbers.push(arguments[i])
@@ -6,4 +6,13 @@ function sum(){
     console.log(numbers)
 }
 
-sum(1,2,3,4,5)
+/*sum(1,2,3,4,5) */
+
+
+
+function avarege(){
+    const soma = sum.apply(null, arguments);
+    return soma / arguments.length;
+}
+
+console.log(avarege(1,2,3,4,5,6,7,8,9,10))
