a532a0c5c98854af891719e490039ed35902a406
Dev. Pablo H. <devpabloh@gmail.com>
2024-09-28 23:10:39 -0300

    att

---
+++ Testes/criandoElementos/javascript.js
@@ -0,0 +1,8 @@
+titulo = document.createElement("h1")
+titulo.textContent = "Meu site criado através de manipulação do DOM"
+document.querySelector(".container").appendChild(titulo)
+
+
+containerPrincipal = document.querySelector(".container")
+containerPrincipal.style.display = "flex"
+containerPrincipal.style.ali
